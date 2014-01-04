29c8d8e214b468ab7a273523918b12072ce89865
Joseph Janiga <Developer@Josephs-Mac-mini.local>
2014-01-04 02:58:52 -0500

    added tests for Door interaction on scrolling maps

--- assets/js/class/Character.js
+++ assets/js/class/Character.js
@@ -20,7 +20,7 @@ define( ['jquery'], function( $ ){
 		this.color = "rgb(0,220,255)";
 		
 		// movement
-		this.movementSpeed = 3;
+		this.movementSpeed = 6;
 		this.direction = 0; // up down left right
 		
 		// collision
